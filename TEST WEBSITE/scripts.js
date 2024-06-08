963b3e9edc8947ac94e570106825bd16c9d9883a
SAMYBOI1224 <jackspedicyteam@gmail.com>
2024-06-08 17:04:01 +0800

    Update scripts.js

--- TEST WEBSITE/scripts.js
+++ TEST WEBSITE/scripts.js
@@ -7,6 +7,12 @@ function selectFlower(flower) {
 }
 
 function selectEmotion(emotion) {
+    const flowerImages = {
+        rose: 'images/rose.jpeg',
+        tulip: 'images/tulip.jpeg',
+        lavender: 'images/lavender.jpeg'
+    };
+
     const quotes = {
         angry: ['Kanino ka galit? Sasakalin natin sila!', 'Yaan mo sila, di natin sila bati.', 'Sumbong mo saken, yari yan.'],
         sad: ['Ice cream is key.', 'Wawawa ulit. AHAHAH', 'Read?'],
@@ -23,6 +29,7 @@ function selectEmotion(emotion) {
     const randomIndex = Math.floor(Math.random() * quotes[emotion].length);
 
     document.getElementById('emotion-section').style.display = 'none';
+    document.getElementById('selected-flower').src = flowerImages[selectedFlower];
     document.getElementById('quote').textContent = quotes[emotion][randomIndex];
     document.getElementById('result-section').style.display = 'block';
 
